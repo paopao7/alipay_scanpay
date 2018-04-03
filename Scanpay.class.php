2c6f4ae379d6e998020f18d96f311f635934cd4e
qianchao_mac <qianchao12345@foxmail.com>
2018-04-03 20:13:56 +0800

    初始化

---
+++ Scanpay.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Org;
+class Scanpay{
+	public function check($data){
+		//以下引入地址有做修改
+		//$data为实际回调参数
+		require_once dirname ( __FILE__ ).DIRECTORY_SEPARATOR.'/f2fpay/config/config.php';
+		require_once dirname ( __FILE__ ).DIRECTORY_SEPARATOR.'/pagepay/service/AlipayTradeService.php';
+
+		$alipaySevice = new \AlipayTradeService($config); 
+		$alipaySevice->writeLog(var_export($data,true));
+		$result = $alipaySevice->check($data);
+
+		return $result;
+	}
+}	
+
+?>
